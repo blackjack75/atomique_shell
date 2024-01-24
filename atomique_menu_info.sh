614561e98dfcaa21db1c52c07c297e16b1d55ffc
Santiago Lema <santiago@lema.org>
2024-01-24 17:57:09 -0300

    About box show a slow teletype effect

--- atomique_menu_info.sh
+++ atomique_menu_info.sh
@@ -1,2 +1,17 @@
-cat $SCRIPT_DIR/atomique_about_text.txt
 
+SLEEPSPEED=0.03
+
+function teletype {
+
+CLEANTXT=$1
+for (( i=0; i<=${#CLEANTXT}; i++ )); do
+ sleep $SLEEPSPEED | echo -ne "${CLEANTXT:$i:1}"
+ done
+
+}
+
+cat "$SCRIPT_DIR/atomique_about_header.txt"
+
+file="$SCRIPT_DIR/atomique_about_text.txt"
+body=$(<"$file")
+teletype "$body"
