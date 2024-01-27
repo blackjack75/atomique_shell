8529119260521446f32e493a13cb7a69c1d05f94
Santiago Lema <santiago@lema.org>
2024-01-27 09:47:02 -0300

    Add gping to dependencies for Linux and FreeBSD

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -6,11 +6,11 @@ platform='unknown'
 unamestr=$(uname)
 if [ "$unamestr" = 'Linux' ]; then
 	   platform='linux'
-sudo apt install fzf ripgrep bat
+sudo apt install fzf ripgrep bat gping
 
    elif [ "$unamestr" = 'FreeBSD' ]; then
 	      platform='freebsd'
-sudo pkg install fzf ripgrep bat
+sudo pkg install fzf ripgrep bat gpingt
    else
     echo "Unsupported platform: $unamestr. Aborting script."
     exit 1
