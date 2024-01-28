5d5d2b1537c01927b710403cc83d117ed4c55856
Santiago Lema <santiago@lema.org>
2024-01-28 15:58:08 -0300

    Update atomique.sh to use login shell for aamenu

--- cmd/atomique.sh
+++ cmd/atomique.sh
@@ -5,7 +5,7 @@
 if tmux list-windows | grep -q "atomique-menu"; then
     tmux select-window -t "atomique-menu"
 else
-    tmux new-window -n "atomique-menu" "zsh -c aamenu"
+    tmux new-window -n "atomique-menu" "$SHELL --login -i -c 'aamenu'"
 fi
 
 return 0
