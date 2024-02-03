d9110c1fdb6e954e72332ee143ee46b8025c0cc7
Santiago Lema <santiago@lema.org>
2024-02-03 18:02:43 -0300

    added dependencies on haiku

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -6,12 +6,15 @@ platform='unknown'
 unamestr=$(uname)
 if [ "$unamestr" = 'Linux' ]; then
 	   platform='linux'
-sudo apt install most pandoc tidy fzf ripgrep bat gping
+sudo apt install most pandoc tidy fzf ripgrep bat pipx ping
 
    elif [ "$unamestr" = 'FreeBSD' ]; then
 	      platform='freebsd'
-sudo pkg install most pandoc tidy fzf ripgrep bat gping
-   else
+sudo pkg install most pandoc tidy fzf ripgrep bat pipx gping
+   elif [ "$unamestr" = 'Haiku' ]; then
+	      platform='haiku'
+pkgman install pandoc most pandoc tidy fzy ripgrep bat pipx gping
+   else	
     echo "Unsupported platform: $unamestr. Aborting script."
     exit 1
 fi
