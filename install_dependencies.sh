8b5cba6c82a00ff4bd27fc5ad06dd2d5e6b0768a
Santiago Lema <santiago@lema.org>
2024-02-03 18:10:41 -0300

    fixed dependencies on Haiku

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -11,15 +11,25 @@ sudo apt install most pandoc tidy fzf ripgrep bat pipx ping
    elif [ "$unamestr" = 'FreeBSD' ]; then
 	      platform='freebsd'
 sudo pkg install most pandoc tidy fzf ripgrep bat pipx gping
+
    elif [ "$unamestr" = 'Haiku' ]; then
 	      platform='haiku'
-pkgman install pandoc most pandoc tidy fzy ripgrep bat pipx gping
+pkgman install pandoc most pandoc tidy fzy ripgrep bat gping
+
    else	
+
     echo "Unsupported platform: $unamestr. Aborting script."
     exit 1
 fi
 
-pipx install toot
-pipx install beautifulsoup4
+#Haiku does not have pipx so install globally I guess
+if command -v pipx &> /dev/null; then
+pipcmd=pipx
+    else
+pipcmd=pip
+fi
+
+$pipcmd install toot
+$pipcmd install beautifulsoup4
 
 cargo install mdcat
