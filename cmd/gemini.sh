b7906089c4cc40c3346f1e7630f28266fec03bc5
Santiago Lema <santiago@lema.org>
2024-02-14 23:52:23 -0300

    added amforas as gemini.sh

---
+++ cmd/gemini.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash   
+ 
+# If script was included from main menu the dir is already defined
+# otherwise point to parent of 'cmd' dir
+if [ -z "$ATOMIQUE_ROOT_DIR" ]
+then
+        export ATOMIQUE_ROOT_DIR="$(dirname "$(readlink -f "$0")")/../"
+fi
+
+tmux rename-window "atomique-gemini"
+amfora
