f3a2a107491ca8ab750583b40eda477c78f74e70
Santiago Lema <santiago@lema.org>
2024-02-25 17:21:44 -0300

    Update SSH connection status message format

--- inc/inc_status_ssh.sh
+++ inc/inc_status_ssh.sh
@@ -6,6 +6,6 @@
    sec=$(( SECONDS % 60 ))                                                                                                                   
    min=$(( SECONDS / 60 ))                                                                                                                   
                                                                                                                                                          
-	echo -ne "\r [ ${min}:${sec} ]  SSH CONNECTION: $1   "
+   echo -ne "\r [ ${min}:${sec} ]  \e[44;33;44m  $1 (ssh)   "
    sleep 1                                                                                                                                               
  done     
