712b104ec0c86a2bf9f725914c8e99e3e6ff6e61
Santiago Lema <santiago@lema.org>
2025-03-15 18:52:10 -0300

    added joplin to main menU

---
+++ cmd/joplin.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash   
+ 
+# If script was included from main menu the dir is already defined
+# otherwise point to parent of 'cmd' dir
+if [ -z "$ATOMIQUE_ROOT_DIR" ]
+then
+        export ATOMIQUE_ROOT_DIR="$(dirname "$(readlink -f "$0")")/../"
+fi
+
+WIN_NAME="atomique-joplin"
+
+#AVOID SHOWING error for fast returning command
+ALLOW_MENU_BACK_NOW=1
+
+# ALWAYS REUSE SAME TMUX WINDOW
+if ! tmux list-windows -F "#{window_name}" | grep -q "$WIN_NAME"; then
+        tmux rename-window "$WIN_NAME"
+        joplin
+else
+        
+	tmux select-window -t "$WIN_NAME"
+
+fi
+
